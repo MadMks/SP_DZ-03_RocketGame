38ddacfaced879778858fccb6d431778b5b0427e
Makc <mks59k@gmail.com>
2018-11-20 15:36:55 +0200

    Create MainForm.

--- RocketGame/Program.cs
+++ RocketGame/Program.cs
@@ -16,7 +16,7 @@ namespace RocketGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
